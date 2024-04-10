b733eb6c1d759a5197f1df28b325ce7773edaafb
dir-FY24ogamikohei <kohei.ogami.h858@dirbato.co.jp>
2024-04-10 12:15:39 +0900

    終了

--- OopBasic/src/company/Department.java
+++ OopBasic/src/company/Department.java
@@ -1,20 +1,20 @@
 package company;
 
-import java.security.PublicKey;
 
-public class department {
-   private final String name;
-   private final String departmentId;
-   private int budget;
+public class Department {
+    private final String name;
+    private final String departmentId;
+    private int budget;
 
-   //コンストラクター　
-   public Department(String name,String departmentId,int budget) {
-       this.name=name;
-       this.departmentId=departmentId
-       this.budget=bedget;
+   // コンストラクター
+   public Department(String name, String departmentId, int budget) {
+       this.name = name;
+       this.departmentId = departmentId;
+       this.budget = budget;
    }
-   //会議を開催する
-   PublicKey voidKey meeting(){
+
+   // 会議を開催する
+   public void meeting() {
        System.out.println("部内会議を開催します。部署：" + name + "、予算：" + budget);
    }
    // nameのgetter
--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,9 +1,9 @@
 package company;
 
-public class Employee {
+public abstract class Employee implements Workable{
 //フィールド
 protected final String name;
-private final department department;
+protected final Department department;
 private final String position;
 private final int employeeId;
 //コンストラクター
@@ -21,11 +21,15 @@ public void report(int times) {
 // 報告メソッド（オーバーロード）
 public void report() {
     report(1);
-    
- // 会議に参加するメソッド
-    public record joinMeeting() {
-        department.meeting();
-        System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" + name);
+}
+    // 会議に参加するメソッド
+    public abstract void  joinMeeting();
+ // 働くメソッドを実装
+    @Override
+    public void work() {
+        System.out.println("正社員として働きます。名前：" + name + slogan);
     }
 }
-}
+
+
+
